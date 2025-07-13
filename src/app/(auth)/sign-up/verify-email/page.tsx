25be9bc72ff14c6c7cdb8f239552dd317dc62598
Shubham Patel <shubham26062002@gmail.com>
2025-07-13 11:21:59 +0530

    Add VerifyEmailPageGuard component.

--- src/app/(auth)/sign-up/verify-email/page.tsx
+++ src/app/(auth)/sign-up/verify-email/page.tsx
@@ -1,14 +1,15 @@
 import { EmailVerificationForm } from "@/components/email-verification-form"
+import { VerifyEmailPageGuard } from "@/components/verify-email-page-guard"
 
 const VerifyEmailPage = () => {
     return (
-        <>
+        <VerifyEmailPageGuard>
             <div className="space-y-1.5">
                 <h1 className="text-3xl font-bold">Cue the Lights!</h1>
                 <p className="text-zinc-400">Enter the code we sent to unlock the stream.</p>
             </div>
             <EmailVerificationForm />
-        </>
+        </VerifyEmailPageGuard>
     )
 }
 
---
+++ src/components/verify-email-page-guard.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import { useRouter } from "next/navigation"
+import { useSignUp } from "@clerk/nextjs"
+import { IconLoader2 } from "@tabler/icons-react"
+
+type VerifyEmailPageGuardProps = {
+    children: React.ReactNode,
+}
+
+export const VerifyEmailPageGuard = ({
+    children,
+}: VerifyEmailPageGuardProps) => {
+    const router = useRouter()
+
+    const { isLoaded, signUp } = useSignUp()
+
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        if (!isLoaded) {
+            return
+        }
+
+        if (signUp.status !== "missing_requirements") {
+            return router.replace("/sign-up")
+        }
+
+        setIsLoading(false)
+    }, [isLoaded, signUp])
+
+    if (isLoading) {
+        return (
+            <IconLoader2 className="size-8 mx-auto transition-all animate-spin" />
+        )
+    }
+
+    return (
+        <>{children}</>
+    )
+}
